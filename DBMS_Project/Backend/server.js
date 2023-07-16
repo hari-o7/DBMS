9c24d4fc350ca2289a79ec3a463397a5887772f7
hari-o7 <hariojoshi@gmail.com>
2023-07-16 23:01:50 +0545

    server side added

---
+++ DBMS_Project/Backend/server.js
@@ -0,0 +1,39 @@
+const { Client } = require("pg");
+
+const express = require("express");
+const app = express();
+
+
+// Database connection details
+const connectionString = "postgres://wfeqgjvl:v11R8OryXqB0d0FMWkcwvpoeJ4oXskJ4@hansken.db.elephantsql.com/wfeqgjvl";
+const client = new Client({
+  connectionString: connectionString,
+});
+
+// Connect to the database
+client.connect();
+
+// Handle form submission
+app.post("/submit-form", (req, res) => {
+    const { name, rollNo, clz } = req.body;
+    // SQL query to insert form data into the table 'form_data'
+  const query = "INSERT INTO form_data (name, rollNo, clz) VALUES ($1, $2, $3)";
+  const values = [name, rollNo, clz];
+
+  // Execute the SQL query to insert form data
+  client.query(query, values, (err, result) => {
+    if (err) {
+      console.error("Error executing query:", err);
+      res.sendStatus(500);
+    } else {
+      console.log("Form data inserted successfully");
+      res.sendStatus(200);
+    }
+  });
+});
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
