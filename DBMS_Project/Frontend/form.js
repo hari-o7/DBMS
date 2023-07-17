1a0d403dd9c46749150d498a372069ec8b49b0f9
hari-o7 <hariojoshi@gmail.com>
2023-07-17 09:03:34 +0545

    connecting F2B

--- DBMS_Project/Frontend/form.js
+++ DBMS_Project/Frontend/form.js
@@ -1,1 +1,40 @@
-console.log('Hello World');
+// Assuming you have a form with the id "myForm"
+const form = document.getElementById("myForm");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); // Prevent the default form submission
+
+  // Retrieve the form data
+  const name = document.getElementById("name").value;
+  const rollNo = document.getElementById("rollNo").value;
+  const clz = document.getElementById("clz").value;
+
+  // Create an object with the form data
+  const formData = {
+    name: name,
+    rollNo: rollNo,
+    clz: clz,
+  };
+
+  // Make an HTTP POST request to the backend endpoint
+  fetch("/submit-form", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  })
+    .then((response) => {
+      if (response.ok) {
+        console.log("Form submitted successfully");
+        // Handle success
+      } else {
+        console.error("Form submission failed");
+        // Handle failure
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      // Handle error
+    });
+});
