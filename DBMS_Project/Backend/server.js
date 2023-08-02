851435c7cb3834fb657b30b9d751f031f1f35e72
hari-o7 <hariojoshi@gmail.com>
2023-08-02 15:00:31 +0545

    comments added

--- DBMS_Project/Backend/server.js
+++ DBMS_Project/Backend/server.js
@@ -1,11 +1,12 @@
 const sql = require("mssql");
-const express = require("express");
-const cors = require("cors");
+const express = require("express"); //handling HTTP requests and responses,
+const cors = require("cors");//cross origin resource sharing
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+//config object to store connection details for connecting to ms sql db
 const config = {
   user: "SA",
   password: "@BeingodockeR7",
@@ -19,19 +20,20 @@ const config = {
 
 app.post("/submit-form", async (req, res) => {
   try {
-    const { name, rollNo, clz } = req.body;
-    const pool = await sql.connect(config);
+    const { name, rollNo, clz } = req.body; //retrieves JSON data sent from client side
+    const pool = await sql.connect(config); //connect to db
 
+    //SQL query to insert the form data into the database
     const query = `INSERT INTO form_tbl (name, rollNo, clz) VALUES (@name, @rollNo, @clz)`;
     await pool
       .request()
-      .input("name", sql.VarChar, name)
+      .input("name", sql.VarChar, name) //line 23 maa we retrieved data into name,rollNo,clz and we use those here.
       .input("rollNo", sql.VarChar, rollNo)
       .input("clz", sql.VarChar, clz)
       .query(query);
 
     console.log("Form data inserted successfully");
-    res.sendStatus(200);
+    res.sendStatus(200); // Send a success status response to the client
 
     await sql.close(); // Close the connection
   } catch (err) {
@@ -39,7 +41,7 @@ app.post("/submit-form", async (req, res) => {
     res.sendStatus(500);
   }
 });
-
+//server will listen for incoming HTTP requests on port 3000. 
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
